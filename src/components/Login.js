1e073bc9d1b3c96c5c2a5c11c5d50e5614bb7307
devshaik444 <mamin172025@bsces.uiu.ac.bd>
2022-06-05 15:05:53 +0600

    first commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,26 +10,36 @@ const inputStyle = {
 
 const Login = () => {
     const navigate = useNavigate();
-    const handleLogin = event => {
-        event.preventDefault();
-        const email = event.target.email.value;
-        const password = event.target.password.value;
+    const handleLogin = e => {
+        e.preventDefault()
+        const email=e.target.email.value;
+        const password=e.target.password.value;
+        console.log({email,password})
 
-        fetch('http://localhost:5000/login', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
+        //login information send/post req to server
+        fetch('http://localhost:5000/login',{
+            method:'POST',
+            headers:{
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
             },
-            body: JSON.stringify({email, password})
+            body: JSON.stringify({email,password})  //always {information to be sent} amn bhabey pathatey hobey  cuz object k stringify kora jay
         })
-        .then(res => res.json())
-        .then(data =>{
+        .then(res=>res.json())      //responsed stringify obj k object a convert
+        .then(data=>{
             if(data.success){
-                localStorage.setItem('accessToken', data.accessToken);
-                navigate('/orders');
+                //'data' tey access token thakbey if user exists ,access token k localStorage store korbo
+                localStorage.setItem('accessToken',data.accessToken)
+                //and user k onno kono page a hoito navigate korey dibo
+                navigate('/orders')
             }
-            console.log(data);
+            else{
+                alert('Wrong name or password')
+            }
+            console.log(data)   //backend thekey data pabo if successfully hit the http://localhost:5000/login url/api
+      
         })
+
     }
 
     return (
--- src/components/Orders.js
+++ src/components/Orders.js
@@ -2,28 +2,26 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Orders = () => {
-    const navigate = useNavigate();
-    const [orders, setOrders] = useState([]);
-
-    useEffect( () =>{
-        fetch('http://localhost:5000/orders', {
-            method: 'GET', 
+    const [orders,setOrders]=useState([])
+    const navigate=useNavigate()
+    //valid access token holey data peye jabo server thekey
+    useEffect(()=>{
+        fetch('http://localhost:5000/orders',{
+            method:"GET",
+            //server a user GET request korar somoy headers a user->accesstoken ta bearer ar sahey server a pathai dissi .
             headers:{
-                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                authorization:`Bearer ${localStorage.getItem('accessToken')}`
             }
         })
-        .then(res => {
-            if(res.status === 401 || res.status===403){
-                navigate('/login');
+        .then(res=>{
+            console.log(res)
+            if(res.status !== 200){
+                navigate('/login')
             }
             return res.json()
         })
-        .then(data =>{
-            console.log(data);
-            setOrders(data);
-        })
-    }, [])
-
+        .then(data=>setOrders(data))
+    },[])
     return (
         <div>
             <h1>This is Orders page: {orders.length}</h1>
